d3714ae493ec430c34f19d7519bbcfe29b6ccc6f
Oliver Widder <oliver.widder@gmail.com>
2019-04-13 12:22:45 +0200

    deploy Sat Apr 13 12:22:45 CEST 2019

---
+++ build/precache-manifest.ce4b0387bea27e4b0dc40bd3885be15d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "646a0e4df8aae42ce229",
+    "url": "/clickdummy/build/static/js/runtime~main.438540f7.js"
+  },
+  {
+    "revision": "22ea02503af19de66fe7",
+    "url": "/clickdummy/build/static/js/main.24c6fc63.chunk.js"
+  },
+  {
+    "revision": "016f1e83c6e3a0c7edc2",
+    "url": "/clickdummy/build/static/js/2.bd483fda.chunk.js"
+  },
+  {
+    "revision": "22ea02503af19de66fe7",
+    "url": "/clickdummy/build/static/css/main.c16736e0.chunk.css"
+  },
+  {
+    "revision": "016f1e83c6e3a0c7edc2",
+    "url": "/clickdummy/build/static/css/2.4e3cc61f.chunk.css"
+  },
+  {
+    "revision": "37b8ba930fea339740d94eb619e98631",
+    "url": "/clickdummy/build/index.html"
+  }
+];
